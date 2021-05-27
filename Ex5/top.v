dce315d38d41b7a8c64822dda2ca88dd75b2ec04
Cloud User <centos@ip-172-31-39-190.eu-west-2.compute.internal>
2021-05-27 13:28:37 +0000

    In progress

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
+// Student Name: Yolanda Ameny
 // Date: 
 //
 //  Description: In this exercise, you need to an air conditioning control system
@@ -12,4 +12,39 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module AC (
+
+// Ports
+input clk,
+input reg [4:0] temperature,
+output heating,
+output cooling);
+
+reg [1:0] state;
+
+assign heating = (state == 0);
+assign cooling = (state == 2);
+
+// Registers & Wires
+always @(posedge clk)
+
+// User Logic
+
+
+if (temperature  <== 18)
+state = 0;
+
+if (temperature > 20)
+state = 1;
+
+if (temperature  >== 22)
+state = 2;
+if (temperature < 20)
+state = 1;
+
+end
+
+endmodule 
 
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
+// Student Name: Yolanda Ameny
 // Date: 
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+// Parameters
+parameter CLK_PERIOD = 10;
+reg clk;
+    reg heating;
+    reg cooling;
+    reg err;
+reg [4:0] temp_1;
+    wire heating;
+wire cooling;
+
+// Clock generation
+initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
